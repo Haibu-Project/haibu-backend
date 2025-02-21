85c2554a8cdd5796f803e02b7536e8181c626893
villarley <santivillarley1010@gmail.com>
2025-02-20 19:39:18 -0600

    fix: added logs

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -12,7 +12,7 @@ export const getAllUsers = async (req: Request, res: Response): Promise<void> =>
     res.status(200).json(users);
   } catch (error) {
     console.log(error);
-    res.status(500).json({ error: "Failed to fetch users" });
+    res.status(500).json({ error: error });
   }
 };
 
