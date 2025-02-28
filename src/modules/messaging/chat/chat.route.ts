9bd4c6570c2f0237c3482403e941bc890b480933
villarley <santivillarley1010@gmail.com>
2025-02-28 13:24:11 -0600

    fix: fixed build logs

--- src/modules/messaging/chat/chat.route.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getMessages } from "./chat.controller";
-
-const router = Router();
-
-router.get("/:user1/:user2", getMessages);
-
-export default router;
