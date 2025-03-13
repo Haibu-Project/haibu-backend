f54b03180b2037df43bbb50c15f6bffb7866e8c0
villarley <santivillarley1010@gmail.com>
2025-03-13 07:13:38 -0600

    Revert "fix: added more space for image uploading"
    This reverts commit 7dbde80a63454b88c8263549f88c8810935b4cd5.

---
+++ src/modules/users/dto/user.dto.ts
@@ -0,0 +1,66 @@
+import { IsNotEmpty, IsString, MinLength, IsOptional, IsEmail } from "class-validator";
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  username!: string;
+
+  @IsNotEmpty()
+  @IsString()
+  walletAddress!: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email!: string;
+
+  @IsNotEmpty()
+  @IsString()
+  name!: string;
+
+  @IsNotEmpty()
+  @IsString()
+  surnames!: string;
+
+
+  @IsOptional()
+  @IsString()
+  image?: string;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+}
+
+export class UpdateUserDto {
+  @IsOptional()
+  @IsString()
+  username?: string;
+
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @IsOptional()
+  @IsString()
+  name?: string;
+
+  @IsOptional()
+  @IsString()
+  surnames?: string;
+
+  @IsOptional()
+  @IsString()
+  image?: string;
+  
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  
+}
+
+export class ValidateUserDto {
+  @IsNotEmpty()
+  @IsEmail()
+  email!: string;
+}
---
+++ src/modules/users/user.service.ts
@@ -0,0 +1,49 @@
+import prisma from "../../database/prisma";
+import { hashPassword, verifyPassword } from "../../utils/auth.utils";
+import { CreateUserDto, UpdateUserDto, ValidateUserDto } from "./dto/user.dto";
+
+export class UserService {
+  static async getAllUsers() {
+    return prisma.user.findMany({
+      select: { id: true, username: true, email: true, createdAt: true, walletAddress: true, description: true, image: true }
+    });
+  }
+
+  static async getUserByUsername(username: string){
+    return prisma.user.findUnique({
+      where: { username },
+      select: { id: true, username: true, email: true, createdAt: true, walletAddress: true, image: true, description: true, posts: true, comments: true, name: true, surnames: true }
+    });
+  }
+
+  static async getUserById(id: string) {
+    return prisma.user.findUnique({
+      where: { id },
+      select: { id: true, username: true, email: true, createdAt: true, walletAddress: true, image: true, description: true }
+    });
+  }
+
+  static async createUser({ username, walletAddress, email, name, surnames, image, description }: CreateUserDto) {
+    return prisma.user.create({
+      data: { username, walletAddress, email, name, surnames, image, description }
+    });
+  }
+
+  static async updateUser(id: string, updateData: UpdateUserDto) {
+    return prisma.user.update({
+      where: { id },
+      data: updateData,
+    });
+  }
+
+  static async deleteUser(id: string) {
+    return prisma.user.delete({ where: { id } });
+  }
+
+  static async validateUser({ email }: ValidateUserDto) {
+    const user = await prisma.user.findUnique({ where: { email } });
+    if (!user) return null;
+
+    return user;
+  }
+}
